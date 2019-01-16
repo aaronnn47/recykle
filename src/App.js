348f7172206a8043678187f31b302ca81692d859
Aaron Kim <aaron@Aarons-MacBook-Pro.local>
2019-01-16 15:12:34 -0700

    changed the title

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ class App extends Component {
       <div className="App">
         
         <div className="header">
-
+          
         </div>
 
         <div className="body">
           <h1>Content and Products Coming soon...</h1>
-          {/* <p>Our greatest glory is not in never falling, but in rising every time we fall. — Confucius</p> */}
         </div>
 
       </div>
