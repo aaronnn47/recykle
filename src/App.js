6e5da35d6034cc95424169a6d60ddb07143dd767
Aaron Kim <aaron@Aarons-MacBook-Pro.local>
2019-01-16 14:21:56 -0700

    updated landing page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        
+        <div className="header">
+
+        </div>
+
+        <div className="body">
+          <h1>Content and Products Coming soon...</h1>
+          {/* <p>Our greatest glory is not in never falling, but in rising every time we fall. — Confucius</p> */}
+        </div>
 
       </div>
     );
