fd06c861ad7f3cdc5a81eb50eb07d914151069ed
Aaron Kim <aaron@Aarons-MacBook-Pro.local>
2019-01-22 17:36:03 -0700

    tried to add the photos but need to resize -no changes made

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react'
 import './Shop.css'
+import blackHoodie from '../../images/black_hoodie.png'
+import beanie from '../../images/beanie.png'
+import mensCamo from '../../images/mens_camo.png'
+import womensT from '../../images/womens_whites.png'
+import purpleHoodie from '../../images/purple_hoodie.png'
 
 class Shop extends Component{
     render(){
         return(
             <div className='shop-body'>
-                <h1>Products Coming Soon...</h1>
+                {/* <img src={blackHoodie} alt=''/>
+                <img src={beanie} alt=''/>
+                <img src={mensCamo} alt=''/>
+                <img src={womensT} alt=''/>
+                <img src={purpleHoodie} alt=''/> */}
+                <p>Products coming soon</p>
             </div>
         )
     }
