cd3e5ddfed6b3c45a921dfa26af3f11e47abac19
Aaron Kim <aaron@Aarons-MacBook-Pro.local>
2019-01-22 18:29:05 -0700

    added featured products list

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './Home.css';
 import image from '../../images/plastic_ocean.jpg'
 import {Link} from 'react-router-dom'
+import mens from '../../images/mens_white.png'
+import hoodie from '../../images/black_hoodie.png'
+import womens from '../../images/womens_whites.png'
 
 class Home extends Component {
   render() {
@@ -24,6 +27,44 @@ class Home extends Component {
         <div className='home-body'>
           <img src={image} alt=''/>
         </div>
+
+        <div className='featured-product'>
+          <h3>Featured Products</h3>
+
+          <div className='featured-images'>
+            <div className='white-shirt'>
+              {/* <h3>White Shirt</h3> */}
+              <img src={mens} alt=''/>
+              {/* <p>Descriptions</p>
+              <p>Descriptions</p>
+              <p>Descriptions</p>
+              <p>Descriptions</p> */}
+            </div>
+
+            <div className='white-shirt'>
+              {/* <h3>Black Hoodie</h3> */}
+              <img src={hoodie} alt=''/>
+              {/* <p>Descriptions</p>
+              <p>Descriptions</p>
+              <p>Descriptions</p>
+              <p>Descriptions</p> */}
+            </div>
+
+            <div className='white-shirt'>
+              {/* <h3>Womens Shirt</h3> */}
+              <img src={womens} alt=''/>
+              {/* <p>Descriptions</p>
+              <p>Descriptions</p>
+              <p>Descriptions</p>
+              <p>Descriptions</p> */}
+            </div>
+
+          </div>
+        </div>
+
+        {/* <div className='product-description'>
+
+        </div> */}
 
       </div>
     );
