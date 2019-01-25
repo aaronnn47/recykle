11d3cac3843ceabfed823088fc090815cb28e566
Aaron Kim <iamaaronkim@gmail.com>
2019-01-25 12:46:40 -0700

    added the shirt to shop

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react'
 import './Shop.css'
+import shirt from '../../images/recykle.png'
 
 
 class Shop extends Component{
     render(){
         return(
             <div className='shop-body'>
-
+                <img src={shirt} alt=''/>
+                <p>New Release</p>
+                <p>100% cotton</p>
+                <p>Pricing coming soon</p>
             </div>
         )
     }
