2f496f65ad0906e0c76bf9c03ef94b3bbdc3181b
Aaron Kim <iamaaronkim@gmail.com>
2019-01-26 13:44:05 -0700

    installed the new shirts

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,16 +1,50 @@
 import React, {Component} from 'react'
 import './Shop.css'
-import shirt from '../../images/recykle.png'
+import shirt from '../../images/recykle_brand.png'
+import anchor from '../../images/recykle_anchor.png'
+import wave from '../../images/recykle_wave.png'
+import Wsurf from '../../images/recykle_womens_surf.png'
+import Wrockhand from '../../images/recykle_rockhand.png'
 
 
 class Shop extends Component{
     render(){
         return(
             <div className='shop-body'>
-                <img src={shirt} alt=''/>
-                <p>New Release</p>
-                <p>100% cotton</p>
-                <p>Pricing coming soon</p>
+                <div className='shop-products'>
+                    <img src={shirt} alt=''/>
+                    <p>New Release</p>
+                    <p>100% cotton</p>
+                    <p>Pricing coming soon</p>
+                </div>
+
+                <div className='shop-products'>
+                    <img src={anchor} alt=''/>
+                    <p>New Release</p>
+                    <p>100% cotton</p>
+                    <p>Pricing coming soon</p>
+                </div>
+
+                <div className='shop-products'>
+                    <img src={wave} alt=''/>
+                    <p>New Release</p>
+                    <p>100% cotton</p>
+                    <p>Pricing coming soon</p>
+                </div>
+
+                <div className='shop-products'>
+                    <img src={Wsurf} alt=''/>
+                    <p>New Release</p>
+                    <p>100% cotton</p>
+                    <p>Pricing coming soon</p>
+                </div>
+
+                <div className='shop-products'>
+                    <img src={Wrockhand} alt=''/>
+                    <p>New Release</p>
+                    <p>100% cotton</p>
+                    <p>Pricing coming soon</p>
+                </div>
             </div>
         )
     }
